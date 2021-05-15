53855a0138880a8f9733023fa6a4a53cd76b06d0
Nathan Andrews <nandrews.programming@gmail.com>
2021-05-15 14:43:18 -0400

    Added crosshair around focused cellNode
    The color of the row and column that contain the focused-on cell change
    color to a darker shade of blue

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Cell = ({ cellData, isStaticCell }) => {
+const Cell = ({ cellData, isStaticCell, onFocus }) => {
   const [currentValue, setCurrentValue] = useState(cellData.value)
 
   return (
@@ -8,6 +8,8 @@ const Cell = ({ cellData, isStaticCell }) => {
       className='cell' 
       value={(currentValue === 0) ? "" : currentValue}
       onChange={event => setCurrentValue(event.target.value)}
+      onFocus={() => onFocus([cellData.rowIndex, cellData.colIndex])}
+      style={{backgroundColor: cellData.color}}
       readOnly={isStaticCell}
     />
   )
--- src/components/SudokuBoard.jsx
+++ src/components/SudokuBoard.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Cell from './Cell'
 import '../styles/board.css'
 
 const SudokuBoard = ({ boardObject }) => {
   const [currentBoard, setCurrentBoard] = useState(boardObject)
+  const [coordinatesOfFocusedCell, setCoordinatesOfFocusedCell] = useState([])
+
+  const getCellCoordinates = (coordinatesArray) => {
+    setCoordinatesOfFocusedCell(coordinatesArray)
+  }
+
+  useEffect(() => {
+    const tempBoard = []
+    const [focusedRow, focusedCol] = coordinatesOfFocusedCell
+    for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+      const tempRow = []
+      for (let colIndex = 0; colIndex < 9; colIndex++) {
+        const cellNode = boardObject[rowIndex][colIndex]
+        if(focusedRow === cellNode.rowIndex || focusedCol === cellNode.colIndex) {
+          tempRow.push({
+            ...cellNode,
+            color: 'rgb(127, 160, 172)'
+          })
+          continue
+        }
+        tempRow.push(cellNode)
+      }
+      tempBoard.push(tempRow)
+    }
+    setCurrentBoard(tempBoard)
+  }, [boardObject, coordinatesOfFocusedCell])
 
   return (
     <div id='sudoku-board'>
@@ -12,6 +38,7 @@ const SudokuBoard = ({ boardObject }) => {
           <Cell key={`${cellNode.rowIndex}${cellNode.columnIndex}`}
             cellData={cellNode}
             isStaticCell={cellNode.isStatic}
+            onFocus={getCellCoordinates}
           />
         )
       )}
