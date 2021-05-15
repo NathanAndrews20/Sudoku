1d82afec3b40b8a718736e6263d2dc38761881db
Nathan Andrews <nandrews.programming@gmail.com>
2021-05-15 12:38:52 -0400

    Added API request functionality
    App.js now fetches a sudoku game, represented as an object containing an
    two-dimensional array which represents the board, and sets the board as
    a state variable.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const App = () => {
+  const [boardFromUrl, setBoardFromUrl] = useState([])
+
+  const getBoardFromUrl = async (url) => {
+    const rawBoard = await fetch(url)
+    const boardAsJSON = await rawBoard.json()
+    return boardAsJSON
+  }
+
+  useEffect(() => {
+    const url = 'https://sugoku.herokuapp.com/board?difficulty=hard'
+    getBoardFromUrl(url)
+      .then(response => {
+      setBoardFromUrl(response.board)
+      })
+  }, [setBoardFromUrl])
+
   return (
     <div className="App">
-      This is the app
+      {JSON.stringify(boardFromUrl)}
     </div>
   )
 }
